673c1f123a3c2622f45b66fbac6877742f10152b
Theo-D <theodessaigne@hotmail.fr>
2025-08-29 13:44:56 +0200

    added base java application package

---
+++ farizasolidaire/src/main/java/com/hbtn/farizasolidaire/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.hbtn.farizasolidaire.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hbtn.farizasolidaire.model.User;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepo extends CrudRepository<User, Integer> {
+
+}
