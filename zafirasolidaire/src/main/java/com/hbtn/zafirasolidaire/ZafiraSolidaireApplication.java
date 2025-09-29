477bcb98d857f797d51ce42f59620b1a4ab34588
Theo-D <theodessaigne@hotmail.fr>
2025-09-29 10:47:38 +0200

    added updated application and application test java files

--- zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/ZafiraSolidaireApplication.java
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/ZafiraSolidaireApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FarizasolidaireApplication {
+public class ZafiraSolidaireApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FarizasolidaireApplication.class, args);
+		SpringApplication.run(ZafiraSolidaireApplication.class, args);
 	}
 
 }
