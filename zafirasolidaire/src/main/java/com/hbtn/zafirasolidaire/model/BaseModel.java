6f3142889f10ea22db4cfa2db774c5f732a4e03f
Theo-D <theodessaigne@hotmail.fr>
2025-09-29 19:08:34 +0200

    Added BaseModel class defining ID and create and update dates as well as its unit testing

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/BaseModel.java
@@ -0,0 +1,62 @@
+package com.hbtn.zafirasolidaire.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@MappedSuperclass
+public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    protected UUID id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "CREATE_DATE")
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name ="UPDATE_DATE")
+    private Date updateDate;
+
+    // ---------- id getter and setter ---------- //
+    public UUID getId() {
+        return id;
+    }
+
+    //Commented setId because JPA handle automatic creation of ID attribute through @GeneratedValue(strategy = GenerationType.UUID)
+    // public void setId(UUID id) {
+    //     this.id = id;
+    // }
+
+    // ---------- createdAt - updatedAt getter and setter ---------- //
+
+    //Protected methods to be access by inheriting classes but not outside of class logic.
+    @PrePersist
+    protected void onCreate(){
+        createDate = new Date();
+        updateDate = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updateDate = new Date();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+}
