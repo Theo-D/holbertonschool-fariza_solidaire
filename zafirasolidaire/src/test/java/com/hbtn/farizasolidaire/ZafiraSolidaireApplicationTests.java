81c42afb8023a321bcb42cd146f935d225a5d6dc
Theo-D <theodessaigne@hotmail.fr>
2025-09-29 10:45:00 +0200

    renamed ZafiraSolidaire

---
+++ zafirasolidaire/src/test/java/com/hbtn/farizasolidaire/ZafiraSolidaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbtn.farizasolidaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarizasolidaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
