bf4a9e45c66275ef68c51fe5cd826bb6e9f75051
Theo-D <theodessaigne@hotmail.fr>
2025-09-30 16:52:58 +0200

    Added photo placeholder Photo model

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/Photo.java
@@ -0,0 +1,14 @@
+package com.hbtn.zafirasolidaire.model;
+
+//Placeholder class waiting to be implemented
+public class Photo {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
