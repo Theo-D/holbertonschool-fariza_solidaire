0d94dfb0552e9b251e8c372fa3a3f662d6e2e317
Theo-D <theodessaigne@hotmail.fr>
2025-09-30 16:34:34 +0200

    feature(user-model): Ceated User model, placeholder Photo model and Unit test

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/User.java
@@ -0,0 +1,83 @@
+package com.hbtn.zafirasolidaire.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="Users")
+public class User extends BaseModel {
+
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    @SuppressWarnings("unused")
+    private String password;
+    private Photo profilePic;
+    private Boolean isAdmin;
+
+    // ---------- firstName getter and setter ---------- //
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    // ---------- lastName getter and setter ---------- //
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    // ---------- emailAddress getter and setter ---------- //
+
+    //TODO: Implement Email verification Service
+    public String getEmail() {
+        return emailAddress;
+    }
+
+    public User setEmail(String emailAddress) {
+        this.emailAddress = emailAddress;
+        return this;
+    }
+
+    // ---------- password setter ---------- //
+    // Test passed - Commenting method
+    // protected String getPasswordForTestingOnly() {
+    //     return password;
+    // }
+
+    //TODO: Implement password hash
+    protected User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    // ---------- profilePic getter and setter ---------- //
+
+    public Photo getProfilePic() {
+        return profilePic;
+    }
+
+    public User setProfilePic(Photo profilePic) {
+        this.profilePic = profilePic;
+        return this;
+    }
+
+    // ---------- isAdmin getter and setter ---------- //
+
+    public Boolean isAdmin() {
+        return isAdmin;
+    }
+
+    protected User setAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+        return this;
+    }
+}
