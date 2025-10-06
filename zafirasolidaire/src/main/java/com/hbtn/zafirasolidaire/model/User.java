c43b2d85abdf1638a6d9a42b0491065278834fbf
Theo-D <theodessaigne@hotmail.fr>
2025-10-06 15:52:32 +0200

    feature(update user model): Fixed unused import in User.java

--- zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/User.java
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/User.java
@@ -10,8 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
