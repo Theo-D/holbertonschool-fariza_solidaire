ceb86fa1573cf213aac1fd799624e7ee40cde947
Theo-D <theodessaigne@hotmail.fr>
2025-10-07 14:12:47 +0200

    feature(create-event-repository): Created EventRepository.

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/repository/EventRepository.java
@@ -0,0 +1,13 @@
+package com.hbtn.zafirasolidaire.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hbtn.zafirasolidaire.model.Event;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, UUID>{
+
+}
