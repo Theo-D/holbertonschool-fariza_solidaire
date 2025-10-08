75fd53e7c9b7a21efa4de6bc3e723473a52303bf
Theo-D <theodessaigne@hotmail.fr>
2025-10-08 19:33:43 +0200

    feature(update partner model): Updated Partner model with jakarta validation annotations.

--- zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/Partner.java
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/Partner.java
@@ -1,10 +1,24 @@
 package com.hbtn.zafirasolidaire.model;
 
-import org.springframework.data.repository.query.parser.Part;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "Partners")
 public class Partner extends BaseModel{
+
+    @NotNull
+    @NotBlank
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "homepage_url", nullable = true)
     private String homepageUrl;
+
+    @Column(name = "logo_url", nullable = true)
     private String logoUrl;
 
     //---------- name getters and setters ----------//
