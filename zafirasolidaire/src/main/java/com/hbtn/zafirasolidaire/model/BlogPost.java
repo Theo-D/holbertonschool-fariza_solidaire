135d22e64e35ef0625b72ec46ea482510b3122f0
Theo-D <theodessaigne@hotmail.fr>
2025-10-08 22:44:22 +0200

    feature(create blogPost model): Created BlogPost model. Created BlogPostTest that implements unit tests for BlogPost.

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/BlogPost.java
@@ -0,0 +1,79 @@
+package com.hbtn.zafirasolidaire.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Blog_post")
+public class BlogPost extends BaseModel {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 100, message = "Title must be between 1 and 100 characters")
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 50, message = "Text body must be at least 50 characters")
+    @Column(name = "text_body", nullable = false)
+    private String textBody;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 50, message = "Author's name must be between 1 and 50 characters")
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "photo_id", unique = true)
+    private Photo photo;
+
+    //---------- Title getters and setters ----------//
+    public String getTitle() {
+    return title;
+    }
+
+    public BlogPost setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    //---------- text body getters and setters ----------//
+    public String getTextBody() {
+        return textBody;
+    }
+
+    public BlogPost setTextBody(String textBody) {
+        this.textBody = textBody;
+        return this;
+    }
+
+    //---------- author getters and setters ----------//
+    public String getAuthor() {
+        return author;
+    }
+
+    public BlogPost setAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+
+    //---------- photo getters and setters ----------//
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public BlogPost setPhoto(Photo photo) {
+        this.photo = photo;
+        return this;
+    }
+}
