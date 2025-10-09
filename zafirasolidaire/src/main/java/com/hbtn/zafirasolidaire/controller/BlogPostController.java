ea313fa7efdf651124c6bf6a0755162519581d0f
Theo-D <theodessaigne@hotmail.fr>
2025-10-09 07:53:47 +0200

    feature(create blogPost controller): Created blogPost controller.

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/controller/BlogPostController.java
@@ -0,0 +1,106 @@
+package com.hbtn.zafirasolidaire.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hbtn.zafirasolidaire.dto.BlogPostDto;
+import com.hbtn.zafirasolidaire.model.BlogPost;
+import com.hbtn.zafirasolidaire.service.BlogPostFacade;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("blog_posts")
+public class BlogPostController {
+    private final BlogPostFacade blogPostFacade;
+
+    @Autowired
+    public BlogPostController(BlogPostFacade blogPostFacade) {
+        this.blogPostFacade = blogPostFacade;
+    }
+
+    // ---------- POST ----------//
+
+    // Save a single blogPost
+    @PostMapping
+    public ResponseEntity<Void> saveBlogPost(@RequestBody @Valid BlogPostDto blogPostDto) {
+        blogPostFacade.createBlogPost(blogPostDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    // Save multiple blogPosts
+    @PostMapping("/batch")
+    public ResponseEntity<Void> saveAllBlogPosts(@RequestBody @Valid List<BlogPostDto> blogPostDtos) {
+        blogPostFacade.createAllBlogPosts(blogPostDtos);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    // Get blogPost by ID
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<BlogPostDto> getBlogPostById(@PathVariable UUID id) {
+        BlogPostDto blogPostDto = blogPostFacade.getBlogPostById(id);
+        return ResponseEntity.ok(blogPostDto);
+    }
+
+    // Check if blogPost exists by ID
+    @GetMapping("/{id}/exists")
+    public ResponseEntity<Boolean> existsById(@PathVariable UUID id) {
+        boolean exists = blogPostFacade.existsById(id);
+        return ResponseEntity.ok(exists);
+    }
+
+    // Get all blogPosts
+    @GetMapping
+    public ResponseEntity<Iterable<BlogPostDto>> getAllBlogPosts() {
+        Iterable<BlogPostDto> blogPosts = blogPostFacade.getAllBlogPosts();
+        return ResponseEntity.ok(blogPosts);
+    }
+
+    // Count blogPosts
+    @GetMapping("/count")
+    public ResponseEntity<Long> countBlogPosts() {
+        long count = blogPostFacade.countBlogPosts();
+        return ResponseEntity.ok(count);
+    }
+
+    // ---------- DELETE ----------//
+
+    // Delete blogPost by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBlogPostById(@PathVariable UUID id) {
+        blogPostFacade.deleteBlogPostById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Delete a single blogPost (full object)
+    @DeleteMapping
+    public ResponseEntity<Void> deleteBlogPost(@RequestBody BlogPost blogPost) {
+        blogPostFacade.deleteBlogPost(blogPost);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Delete all blogPosts
+    @DeleteMapping("/all")
+    public ResponseEntity<Void> deleteAllBlogPosts() {
+        blogPostFacade.deleteAllBlogPosts();
+        return ResponseEntity.noContent().build();
+    }
+
+    // Delete specific blogPosts
+    @DeleteMapping("/batch")
+    public ResponseEntity<Void> deleteAllBlogPostsFromlist(@RequestBody List<BlogPost> blogPosts) {
+        blogPostFacade.deleteAllBlogPosts(blogPosts);
+        return ResponseEntity.noContent().build();
+    }
+}
