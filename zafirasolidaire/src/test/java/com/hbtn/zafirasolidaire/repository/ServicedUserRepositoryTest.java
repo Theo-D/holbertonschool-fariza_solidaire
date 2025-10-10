23b55b3f1cf076f06f5bd06af5230120119d8519
Theo-D <theodessaigne@hotmail.fr>
2025-10-10 10:50:20 +0200

    feature/create-servicedUser-repository-test: Created ServicedUSerRepositoryTest.

--- zafirasolidaire/src/test/java/com/hbtn/zafirasolidaire/repository/ServicedUserRepositoryTest.java
+++ zafirasolidaire/src/test/java/com/hbtn/zafirasolidaire/repository/ServicedUserRepositoryTest.java
@@ -2,8 +2,6 @@ package com.hbtn.zafirasolidaire.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.UUID;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
