191b98e895effef012e21e2ecf843ad81f6fe20f
Theo-D <theodessaigne@hotmail.fr>
2025-10-10 13:40:54 +0200

    feature(create clothing model): Created classes for the whole Clothing entitiy (model, facade, repo, facade, config). Testing to come.

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/service/ClothingFacade.java
@@ -0,0 +1,43 @@
+package com.hbtn.zafirasolidaire.service;
+
+import org.springframework.stereotype.Service;
+
+import static com.hbtn.zafirasolidaire.model.Clothing.CLOTHING_UUID;
+
+import com.hbtn.zafirasolidaire.model.Clothing;
+import com.hbtn.zafirasolidaire.repository.ClothingRepository;
+
+@Service
+public class ClothingFacade {
+    private final ClothingRepository clothingRepository;
+
+    public ClothingFacade(ClothingRepository clothingRepository){
+        this.clothingRepository = clothingRepository;
+    }
+
+    // ---------- Repository Services ---------- //
+
+    public Clothing getClothing() {
+        Clothing clothing = clothingRepository.findById(CLOTHING_UUID)
+                                              .orElseThrow(() -> new IllegalArgumentException("Clothing not found"));
+
+        return clothing;
+    }
+
+    public void updateClothingWeight(int weight) {
+        Clothing clothing = getClothing();
+
+        clothing.setWeight(weight);
+
+        clothingRepository.save(clothing);
+    }
+
+    public void incrementClothingWeight(int addedWeight) {
+        Clothing clothing = getClothing();
+        int weight = clothing.getWeight();
+
+        clothing.setWeight(weight + addedWeight);
+
+        clothingRepository.save(clothing);
+    }
+}
