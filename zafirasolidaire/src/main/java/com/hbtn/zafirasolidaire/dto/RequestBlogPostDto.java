ea54a6bd92219f0c8337bd83e93683716851bd6b
Theo-D <theodessaigne@hotmail.fr>
2025-10-15 15:01:02 +0200

    development/update-RequestBlogPost-dto: Updated RequestBlogPostDto with minimum size for textBody.

--- zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/dto/RequestBlogPostDto.java
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/dto/RequestBlogPostDto.java
@@ -2,6 +2,7 @@ package com.hbtn.zafirasolidaire.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class RequestBlogPostDto {
 
@@ -11,6 +12,7 @@ public class RequestBlogPostDto {
 
     @NotNull
     @NotBlank
+    @Size(min = 50)
     private String textBody;
 
     @NotNull
