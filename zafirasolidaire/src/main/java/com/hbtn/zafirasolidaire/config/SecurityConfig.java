78d878015715ddb32da84c3ace487018580947e8
Theo-D <theodessaigne@hotmail.fr>
2025-10-16 16:00:23 +0200

    feature(add role based controller access): Added fine grained role based controller access and bean to handle cors request.

--- zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/config/SecurityConfig.java
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hbtn.zafirasolidaire.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -22,6 +28,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
         private final JwtFilter jwtFilter;
@@ -41,6 +48,7 @@ public class SecurityConfig {
         SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
                 return http
                         .csrf(customizer -> customizer.disable())
+                        .cors(Customizer.withDefaults())
                         .authorizeHttpRequests(request -> request
                                 .requestMatchers(AUTHORIZED_PATHS).permitAll()
                                 .anyRequest().authenticated())
@@ -75,5 +83,18 @@ public class SecurityConfig {
         @Bean
         AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
                 return authConfig.getAuthenticationManager();
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://localhost:5173"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(List.of("*"));
+                config.setAllowCredentials(true);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", config);
+                return source;
         }
 }
