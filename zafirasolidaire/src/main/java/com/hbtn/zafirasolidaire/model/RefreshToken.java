1ba33f4ac6b0e9dd431aba68ec0ade5d759d7232
Theo-D <theodessaigne@hotmail.fr>
2025-10-17 12:04:31 +0200

    feature(create refresh token model): Created RefreshToken model.

---
+++ zafirasolidaire/src/main/java/com/hbtn/zafirasolidaire/model/RefreshToken.java
@@ -0,0 +1,51 @@
+package com.hbtn.zafirasolidaire.model;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="refreshTokens")
+public class RefreshToken extends BaseModel{
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expirationDate;
+
+    public User getUser() {
+        return user;
+    }
+
+    public RefreshToken setUSer(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public RefreshToken setUSer(String token) {
+        this.token = token;
+        return this;
+    }
+
+    public Instant getExpirationDate() {
+        return expirationDate;
+    }
+
+    public RefreshToken setExpirationDate(Instant expirationDate) {
+        this.expirationDate = expirationDate;
+        return this;
+    }
+}
