bf97a45ce562c9cebd1d13356896538786994215
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-10-20 16:39:59 +0200

    Fixes server connection

---
+++ zafirasolidaire-website/tailwind.config.js
@@ -0,0 +1,11 @@
+import daisyui from 'daisyui'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+}
