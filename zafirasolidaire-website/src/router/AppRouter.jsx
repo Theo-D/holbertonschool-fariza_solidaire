7a51ba4f0e0533bd7c248468f62912b79743bf81
Theo-D <theodessaigne@hotmail.fr>
2025-10-20 17:50:17 +0200

    feature(create route structure): Creted router/AppRouter.jsx, pages/Home.jsx pages/Login.jsx and updated App.jsx accordingly.

---
+++ zafirasolidaire-website/src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home"
+
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
