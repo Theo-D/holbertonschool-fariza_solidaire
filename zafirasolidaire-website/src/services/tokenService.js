ca5074eceb492ac01e40451adca93f8dda58d3eb
Theo-D <theodessaigne@hotmail.fr>
2025-10-21 18:05:44 +0200

    development(update token service): Updated tokenService, removing the base URL part of the request

--- zafirasolidaire-website/src/services/tokenService.js
+++ zafirasolidaire-website/src/services/tokenService.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
 export const refreshRequest = async () => {
-  const response = await axios.post(
-    "http://localhost:8080/auth/refresh",
-    {},
-    { withCredentials: true }
-  );
+  const response = await axios.post("/auth/refresh",{},{ withCredentials: true });
   return response.data; // { accessToken }
 };
 
 export const logoutRequest = async () => {
-  await axios.post(
-    "http://localhost:8080/auth/logout",
-    {},
-    { withCredentials: true }
-  );
+  await axios.post("/auth/logout",{},{ withCredentials: true });
 };
