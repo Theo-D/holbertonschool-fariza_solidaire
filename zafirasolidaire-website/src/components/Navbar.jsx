51f5b768467cf1d8a4f1cfa9d6bbd4fe4d19a1d6
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-10-22 18:06:25 +0200

    Finishes the look of the Home Page

--- zafirasolidaire-website/src/components/Navbar.jsx
+++ zafirasolidaire-website/src/components/Navbar.jsx
@@ -4,16 +4,20 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbar bg-base-200 shadow-md px-6">
+      <div className="flex-0 bg-42AAE1"/>
+      <div className="flex-0 bg-E82B89"/>
+      <div className="flex-0 bg-FCD916"/>
       <div className="flex-1">
         <Link to="/" className="text-xl font-bold text-primary">
-          MyWebsite
+          Zafira Solidaire
         </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/about">A propos</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><button className="button bg-blue-600 text-white hover:bg-blue-800"><Link to="/login">Se connecter</Link></button></li>
         </ul>
       </div>
     </div>
--- zafirasolidaire-website/tailwind.config.js
+++ zafirasolidaire-website/tailwind.config.js
@@ -1,11 +1,34 @@
 import daisyui from 'daisyui'
+import plugin from 'tailwindcss/plugin'
 
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+      'brand-blue': '#42AAE1',
+      'brand-pink': '#E82B89',
+      'brand-yellow': '#FCD916',
+    }
+    },
   },
   plugins: [
     daisyui,
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.bg-linear-to-br': {
+          'background-image': 'linear-gradient(to bottom right, var(--tw-gradient-stops))',
+        },
+        '.bg-linear-to-tr': {
+          'background-image': 'linear-gradient(to top right, var(--tw-gradient-stops))',
+        },
+        '.bg-linear-to-bl': {
+          'background-image': 'linear-gradient(to bottom left, var(--tw-gradient-stops))',
+        },
+        '.bg-linear-to-tl': {
+          'background-image': 'linear-gradient(to top left, var(--tw-gradient-stops))',
+        },
+      })
+    })
   ],
 }
