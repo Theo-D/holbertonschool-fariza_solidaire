bd910e34bbf74c346879c26fcfb25f312afb123a
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-10-24 17:16:15 +0200

    Fixes user services by removing duplicate of an ansync function

--- zafirasolidaire-website/src/services/user_services/userApi.js
+++ zafirasolidaire-website/src/services/user_services/userApi.js
@@ -1,38 +1,34 @@
 import api from "../api"
 
-export const getUsers = async function() {
+export const getUsers = async function () {
   return await api.get("/users");
 };
 
-export const getUserById = async function(id) {
+export const getUserById = async function (id) {
   return await api.get(`/users/${id}`);
 };
 
-export const countServicedUsers = async function() {
+export const countServicedUsers = async function () {
   return await api.get("/serviced_users/count");
 };
 
-export const countServicedUsers = async function() {
-  return await api.get("/serviced_users/count");
-};
-
-export const saveUser = async function(user) {
+export const saveUser = async function (user) {
   return await api.post("/users", user)
 }
 
-export const updateUser = async function(id, user){
+export const updateUser = async function (id, user) {
   return await api.put(`/users/${id}`, user)
 }
 
-export const setUserPhoto = async function(id) {
+export const setUserPhoto = async function (id) {
   return await api.post(`/users/${id}/photo`);
 };
 
-export const deleteUserById = async function(id) {
+export const deleteUserById = async function (id) {
   return await api.delete(`/users/${id}`);
 };
 
-export const createServicedUser = async function(userId) {
+export const createServicedUser = async function (userId) {
   return await api.post(`serviced_users`, { userId });
 }
 
