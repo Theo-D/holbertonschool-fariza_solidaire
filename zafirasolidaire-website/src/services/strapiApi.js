ade09e42147763191a6c8f14ffdb8b8e8642246e
Theo-D <theodessaigne@hotmail.fr>
2025-10-26 15:19:03 +0100

    feature(fix-blogPost-delete)

--- zafirasolidaire-website/src/services/strapiApi.js
+++ zafirasolidaire-website/src/services/strapiApi.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const STRAPI_TOKEN = "b8a1ebd9ac87c792536172cb68f5f0b6606316298554a29c781e3d76832c9578946dc5c865f73f512a33ce3257fe75680dbe056ebb3adc9676f0c7e387870fba81e3f2f89bb8bd48d1f86d8e9265969120a4c5776bd6da72192a054ef25f94ab488fec152cefa22618e047717ee9c03360413df9796c520255ea02a4d05eecd5";
 
 const api = axios.create({
   baseURL: "http://localhost:1337/api",
+  headers: {
+    Authorization: `Bearer ${STRAPI_TOKEN}`,
+  },
   withCredentials: true,
 });
 
