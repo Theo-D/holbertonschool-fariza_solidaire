52e019fd78a9105af371ffeb380adfd4a33e8838
Theo-D <theodessaigne@hotmail.fr>
2025-11-01 12:05:45 +0100

    feature(contact info footer): Added contact info into the footer.

---
+++ zafirasolidaire-website/src/components/ClockSvg.jsx
@@ -0,0 +1,18 @@
+export default function ClockSvg() {
+    return(
+        <div className="w-8 h-8">
+            <svg viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <path d="M512 512m-426.666667 0a426.666667 426.666667 0 1 0 853.333334 0 426.666667 426.666667 0 1 0-853.333334 0Z" fill="#00ACC1"></path>
+                    <path d="M512 512m-341.333333 0a341.333333 341.333333 0 1 0 682.666666 0 341.333333 341.333333 0 1 0-682.666666 0Z" fill="#EEEEEE"></path>
+                    <path d="M490.666667 234.666667h42.666666v277.333333h-42.666666z" fill=""></path>
+                    <path d="M667.413333 632.618667L632.746667 667.306667l-138.752-138.752 34.688-34.709334z" fill=""></path>
+                    <path d="M512 512m-42.666667 0a42.666667 42.666667 0 1 0 85.333334 0 42.666667 42.666667 0 1 0-85.333334 0Z" fill=""></path>
+                    <path d="M512 512m-21.333333 0a21.333333 21.333333 0 1 0 42.666666 0 21.333333 21.333333 0 1 0-42.666666 0Z" fill="#00ACC1"></path>
+                </g>
+            </svg>
+        </div>
+    );
+}
